87ec1f7b108581c396210f42cbb7844301b3c54e
Cole <cole2247@gmail.com>
2022-04-14 22:20:16 -0400

    fist commit with some configs, more to do

---
+++ xrandr/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1600x900 --pos 0x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 3520x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 5440x0 --rotate normal
